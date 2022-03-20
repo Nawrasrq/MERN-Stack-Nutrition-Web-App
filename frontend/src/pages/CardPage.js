840feb0c80e45b6acf985c8d54af2ffddf66af24
root <root@DESKTOP-234IJKO.localdomain>
2022-03-20 13:23:56 -0400

    Nawras API Commit

---
+++ frontend/src/pages/CardPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+/*import PageTitle from '../components/PageTitle';
+import LoggedInName from '../components/LoggedInName';
+import CardUI from '../components/CardUI';*/
+const CardPage = () =>
+{
+    return(
+        <div>
+
+        </div>
+    );
+}
+export default CardPage;
---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+/*import PageTitle from '../components/PageTitle';
+import Login from '../components/Login';*/
+const LoginPage = () =>
+{
+    return(
+      <div>
+        
+      </div>
+    );
+};
+export default LoginPage;
