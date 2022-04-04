7392b264348762356156234ca59c4274e10dc7f0
TiddlyWalnut <dillgarcia18@gmail.com>
2022-04-04 13:59:26 -0400

    filtersearch update

--- api.js
+++ api.js
@@ -367,11 +367,11 @@ exports.setApp = function ( app, client )
        res.send(res.meal.Name);
     });
 
-    app.get('/api/filtersearch/:name/', async (req, res, next) => {
+    app.get('/api/filtersearch/:name/:UserId', async (req, res, next) => {
 
         let partialToMatchName = new RegExp(req.params.name,'i');
 
-        Meal.find({Name: partialToMatchName}, function(err, foundMeal) {
+        Meal.find({Name: partialToMatchName}, {UserId: req.params.UserId}, function(err, foundMeal) {
             if (foundMeal != '') {
                 res.send(foundMeal);
             } else {
