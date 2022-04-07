58d3161d67335c24f06bd43bf1474f02f1387242
TiddlyWalnut <dillgarcia18@gmail.com>
2022-04-07 14:42:09 -0400

    filtersearch update

--- api.js
+++ api.js
@@ -393,8 +393,8 @@ exports.setApp = function ( app, client )
 
         Meal.find({Name: partialToMatchName}, function(err, foundMeal) {
             if (foundMeal != '') {
-                console.log(foundMeal[0][0])
-                res.send(foundMeal);
+                //res.send(foundMeal);
+                res.send(foundMeal[0]);
             } else {
                 res.send("No meal matching that name was found.");
             }
