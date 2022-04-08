705ac876dbfd93cba7e4f566564cfd5501403910
Garrett Spears <garrettspears12@gmail.com>
2022-04-08 00:44:35 -0400

    Improved feedback from add food form.

--- frontend/src/components/AddMeal.js
+++ frontend/src/components/AddMeal.js
@@ -19,6 +19,12 @@ function AddMeal()
 
     var storage = require('../tokenStorage.js');
 
+    // This function just resets the displayed message whenever the user starts typing again in any of the input text boxes.
+    function clearMessage()
+    {
+        setMessage("");
+    }
+
     const doAddMeal = async event => 
     {
         // create object from text boxes and make JSON 
@@ -55,7 +61,7 @@ function AddMeal()
             }
             else
             {
-                setMessage('');
+                setMessage("\"" + foodName.value + "\"" + " successfully added to your list of foods.");
             }
         }
         catch(e)
@@ -69,15 +75,15 @@ function AddMeal()
     <div id="addMealDiv">
         <form onSubmit={doAddMeal}>
             <span id="inner-title">Add Meal (required fields indicated by *)</span><br />
-            <input type="text" id="foodName" placeholder="Food Name" ref={(c) => foodName = c} /> *<br />
-            <input type="number" id="calories" placeholder="Calories" ref={(c) => calories = c} /> *<br />
-            <input type="number" id="protein" placeholder="Protein" ref={(c) => protein = c} /><br />
-            <input type="number" id="carbs" placeholder="Carbohydrates" ref={(c) => carbs = c} /><br />
-            <input type="number" id="fat" placeholder="Fat" ref={(c) => fat = c} /><br />
-            <input type="number" id="fiber" placeholder="Fiber" ref={(c) => fiber = c} /><br />
-            <input type="number" id="sugar" placeholder="Sugar" ref={(c) => sugar = c} /><br />
-            <input type="number" id="sodium" placeholder="Sodium" ref={(c) => sodium = c} /><br />
-            <input type="number" id="cholesterol" placeholder="Cholesterol" ref={(c) => cholesterol = c} /><br />
+            <input type="text" id="foodName" placeholder="Food Name" onKeyUp={clearMessage} ref={(c) => foodName = c} /> *<br />
+            <input type="number" id="calories" placeholder="Calories" onKeyUp={clearMessage} ref={(c) => calories = c} /> *<br />
+            <input type="number" id="protein" placeholder="Protein" onKeyUp={clearMessage} ref={(c) => protein = c} /><br />
+            <input type="number" id="carbs" placeholder="Carbohydrates" onKeyUp={clearMessage} ref={(c) => carbs = c} /><br />
+            <input type="number" id="fat" placeholder="Fat" onKeyUp={clearMessage} ref={(c) => fat = c} /><br />
+            <input type="number" id="fiber" placeholder="Fiber" onKeyUp={clearMessage} ref={(c) => fiber = c} /><br />
+            <input type="number" id="sugar" placeholder="Sugar" onKeyUp={clearMessage} ref={(c) => sugar = c} /><br />
+            <input type="number" id="sodium" placeholder="Sodium" onKeyUp={clearMessage} ref={(c) => sodium = c} /><br />
+            <input type="number" id="cholesterol" placeholder="Cholesterol" onKeyUp={clearMessage} ref={(c) => cholesterol = c} /><br />
             <input type="submit" id="addMealButton" class="buttons" value = "Add Meal" onClick={doAddMeal} /><br />
             <span id="addMealResult">{message}</span>
         </form>
