e287e3ccd642a7c6d610797b591ccf5ef723f9bf
Garrett Spears <garrettspears12@gmail.com>
2022-04-30 15:58:23 -0400

    Cleaned up use of state variables on main page.

--- frontend/src/components/ListTrackedFoods.js
+++ frontend/src/components/ListTrackedFoods.js
@@ -1,84 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function ListTrackedFoods(props)
 {
-    const [foods, setFoods] = new useState([]);
-    const [message, setMessage] = new useState('');
-    const [storedDate, setStoredDate] = new useState(null);
-
-    async function doRetrieveTrackedFoods()
-    {
-        let _ud = localStorage.getItem('user_data');
-        let ud = JSON.parse(_ud);
-        let userId = ud.id;
-
-        // Get jwt token from local storage
-        let storage = require('../tokenStorage.js');
-        let tok = storage.retrieveToken();
-
-        let date = props.date.toLocaleDateString();
-
-        var obj = {
-            UserId:userId,
-            jwtToken:tok,
-            Date:date
-        }
-
-        var js = JSON.stringify(obj);
-        try
-        {     
-            // Send off package to api and await response 
-            var bp = require('./Path.js');
-            const response = await fetch(bp.buildPath('api/retrievetracked/'),{method:'POST', body:js, headers:{'Content-Type': 'application/json'}});
-            var res = JSON.parse(await response.text());
-
-            if (!res.jwtToken)
-            {
-                setMessage(res.error);
-                return;
-            }
-            // Token was expired
-            else if (res.jwtToken.length === 0)
-            {
-                alert("Your session has expired, please log in again.");
-                localStorage.removeItem("user_data")
-		        window.location.href = '/';
-                return;
-            }
-
-            storage.storeToken(res.jwtToken);
-            
-            if (res.error.length > 0)
-            {
-                setMessage(res.error);
-                setFoods([]);
-            }
-            else
-            {
-                setFoods(res.trackedFoods);
-            }
-        }
-        catch(e)
-        {
-            console.log(e.toString());
-            return;
-        }
-    }
-
-    // Initialize list of tracked foods for this date and only
-    // retrieve foods again if date has changed (prevents infinite re-render loop)
-    if (props.date.toLocaleDateString() !== storedDate)
-    {
-        setMessage("");
-        setStoredDate(props.date.toLocaleDateString());
-        doRetrieveTrackedFoods();
-    }
-
+    // Simply just displays all the foods retrieved in a nice list format
     return(
         <div>
-            <span>{message}</span>
             <ul>
-                {foods.map(food => (
+                {props.foods.map(food => (
                     <li key={food._id}>
                         <span>{food.MealId}</span>
                         <span> Quantity:{food.Quantity}</span><br/>
@@ -88,5 +16,4 @@ function ListTrackedFoods(props)
         </div>
     );
 };
-
 export default ListTrackedFoods;
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import NavigationBar from '../components/NavigationBar';
 import Logout from '../components/Logout';
@@ -7,6 +7,9 @@ import ListTrackedFoods from '../components/ListTrackedFoods';
 function MainPage()
 {
     const [selectedDate, setSelectedDate] = useState(new Date());
+    const [message, setMessage] = useState('');
+    const [foods, setFoods] = new useState([]);
+
 
     // Number of milliseconds in a day
     const msInDay = 86400000;
@@ -15,7 +18,7 @@ function MainPage()
     {     
         let newDate = new Date(selectedDate.valueOf() - msInDay);
         setSelectedDate(newDate);
-
+        doRetrieveTrackedFoods();
     }
     
     function incrementDay()
@@ -31,7 +34,77 @@ function MainPage()
         }
 
         setSelectedDate(newDate);
+        doRetrieveTrackedFoods();
     }
+
+    async function doRetrieveTrackedFoods()
+    {
+        // clear message since new message is ab to be displayed
+        setMessage("");
+
+        let _ud = localStorage.getItem('user_data');
+        let ud = JSON.parse(_ud);
+        let userId = ud.id;
+
+        // Get jwt token from local storage
+        let storage = require('../tokenStorage.js');
+        let tok = storage.retrieveToken();
+
+        let date = selectedDate.toLocaleDateString();
+
+        var obj = {
+            UserId:userId,
+            jwtToken:tok,
+            Date:date
+        }
+
+        var js = JSON.stringify(obj);
+        try
+        {     
+            // Send off package to api and await response 
+            var bp = require('../components/Path.js');
+            const response = await fetch(bp.buildPath('api/retrievetracked/'),{method:'POST', body:js, headers:{'Content-Type': 'application/json'}});
+            var res = JSON.parse(await response.text());
+
+            if (!res.jwtToken)
+            {
+                setMessage(res.error);
+                return;
+            }
+            // Token was expired
+            else if (res.jwtToken.length === 0)
+            {
+                alert("Your session has expired, please log in again.");
+                localStorage.removeItem("user_data")
+		        window.location.href = '/';
+                return;
+            }
+
+            storage.storeToken(res.jwtToken);
+            
+            if (res.error.length > 0)
+            {
+                setMessage(res.error);
+                setFoods([]);
+                return;
+            }
+            else
+            {
+                setFoods(res.trackedFoods);
+            }
+            let trackedFoods = res.trackedFoods;
+        }
+        catch(e)
+        {
+            console.log(e.toString());
+            return;
+        }
+    }
+
+    // Initialize list of trackedfoods on page when page first renders
+    useEffect(() => {
+        doRetrieveTrackedFoods();
+    }, []);
 
     return(
         <div >
@@ -41,7 +114,8 @@ function MainPage()
             <span>{selectedDate.toDateString()}</span>
             <Button variant='primary' className='m-3' disabled={selectedDate.toLocaleDateString() === (new Date()).toLocaleDateString()} onClick={incrementDay} > Next </Button><br/>
 
-            <ListTrackedFoods date={selectedDate} />
+            <span>{message}</span>
+            <ListTrackedFoods foods={foods} />
 
             <Button variant='primary' className='m-3' href="/Main/AddToDailyConsumption" > Add To Your Daily Consumption </Button>
         </div>
