06da05cc506dad4ea9899a0c34018136826e3b31
TiddlyWalnut <dillgarcia18@gmail.com>
2022-05-03 01:56:58 -0400

    editmeal fix

--- api.js
+++ api.js
@@ -521,26 +521,37 @@ exports.setApp = function ( app, client )
         //search for meal
         const meal = await Meal.findById(id);
 
-        if(meal){
-            if(Name) meal.Name = Name;
-            if(Calories) meal.Calories = Calories;
-            if(Protein) meal.Protein = Protein;
-            if(Carbs) meal.Carbs = Carbs;
-            if(Fat) meal.Fat = Fat;
-            if(Fiber) meal.Fiber = Fiber;
-            if(Sugar) meal.Sugar = Sugar;
-            if(Sodium) meal.Sodium = Sodium;
-            if(Cholesterol) meal.Cholesterol = Cholesterol;
-
-            await meal.save();
-
-            //success
-            error = "";
+        // Check if anything will actually be updated
+        if (!Name && !Calories && !Protein
+            && !Carbs && !Fat && !Fiber
+            && !Sugar && !Sodium && !Cholesterol)
+        {
+            error = "No fields were edited.";
             ret = {meal: meal, error: error, jwtToken: refreshedToken};
         }
-        else{
-            error = "meal not found";
-            ret = {error: error, jwtToken: refreshedToken};
+        else
+        {
+            if(meal){
+                if(Name) meal.Name = Name;
+                if(Calories) meal.Calories = Calories;
+                if(Protein) meal.Protein = Protein;
+                if(Carbs) meal.Carbs = Carbs;
+                if(Fat) meal.Fat = Fat;
+                if(Fiber) meal.Fiber = Fiber;
+                if(Sugar) meal.Sugar = Sugar;
+                if(Sodium) meal.Sodium = Sodium;
+                if(Cholesterol) meal.Cholesterol = Cholesterol;
+
+                await meal.save();
+
+                //success
+                error = "";
+                ret = {meal: meal, error: error, jwtToken: refreshedToken};
+            }
+            else{
+                error = "meal not found";
+                ret = {error: error, jwtToken: refreshedToken};
+            }
         }
         
         res.status(200).json(ret);
